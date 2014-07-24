dcfcb84f5f784af5fc08312ee49fa221eb2469b6
FukuiRetu <s0232101@gmail.com>
2014-07-25 07:34:23 +0900

    Modify

---
+++ app/controllers/event_list_controller.rb
@@ -0,0 +1,4 @@
+class EventListController < ApplicationController
+  def show
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    # render :action => "before_login"
+    render :action => "index"
+  end
+  def before_login
   end
 end
