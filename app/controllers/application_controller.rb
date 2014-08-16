2109da94bd22b5940a6e3a7f4de9755bc0121709
FukuiRetu <s0232101@gmail.com>
2014-08-16 09:57:15 +0900

    Modify before_filter => before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :check_login, :except => [:index, :callback, :destroy]
+  before_action :check_login, :except => [:index, :callback, :destroy]
   helper_method :logged_in?
 
   def check_login
     unless logged_in?
       redirect_to root_path
     end
-    
+
     current_user
   end
 
