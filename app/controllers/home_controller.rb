3c15b83e5a5d1e9ec1f63bea11b87c86fdb830e4
FukuiRetu <s0232101@gmail.com>
2014-09-01 00:49:15 +0900

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,15 @@ class HomeController < ApplicationController
 
     current_user
 
-    @infos = Rails.cache.fetch(:infos, :expires_in => 5.minutes) do
+    @infos = Rails.cache.fetch("infos", expires_in: 5.minutes) do
       Information.enable.order(:id => :desc)
     end
 
-    @new_events = Rails.cache.fetch(:new_events, :expires_in => 5.minutes) do
+    @new_events = Rails.cache.fetch("new_events", expires_in: 5.minutes) do
       Event.all.order(:created_at => :desc).limit(6)
     end
 
-    @tags = Rails.cache.fetch(:tags, :expires_in => 5.minutes) do
+    @tags = Rails.cache.fetch("tags", expires_in: 5.minutes) do
       Tag.all.order(:content => :asc)
     end
 
