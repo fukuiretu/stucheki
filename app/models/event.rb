22168dd87d795ac33ef668a1fb27549d5f7e5a4a
FukuiRetu <s0232101@gmail.com>
2014-09-05 12:46:44 +0900

    Add enum

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,6 +19,8 @@ class Event < ActiveRecord::Base
   has_many :cheki_events
   has_many :users, through: :cheki_events
 
+  enum origin_type: {atnd: 1, compass: 2, door_keeper: 3, zussar: 4}
+
   scope :merge_cheki_events, -> {
     joins("LEFT OUTER JOIN cheki_events on events.id = cheki_events.event_id")
       .select("events.*, cheki_events.event_id")
